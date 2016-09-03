0a0cfad1eba3e73cc83e5382549b2c2fd41caea8
sharkman-jhones <sharkman.jhones@gmail.com>
2016-09-03 15:28:16 -0500

    Finished Assignment

--- assets/Javascript/program.js
+++ assets/Javascript/program.js
@@ -1,4 +1,4 @@
-$('.animalButton').on('click', function(){
+$(document).on('click', '.animalButton', function(){
 	var p = $(this).data('animal');
 
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+p+"&api_key=dc6zaTOxFJmzC&limit=10";
@@ -13,15 +13,46 @@ $('.animalButton').on('click', function(){
 			var rating = results[i].rating;
 			var p = $('<p>').text("Rating: "+ rating);
 			var animalImage = $('<img>');
-			animalImage.attr('src', results[i].images.fixed_height.url);
+			animalImage.attr('src', results[i].images.fixed_height_still.url);
+			animalImage.attr('data-still', results[i].images.fixed_height_still.url);
+			animalImage.attr('data-animate', results[i].images.fixed_height.url);
+			animalImage.attr('data-state', 'still');
+			console.log(results[i]);
 
 			gifDiv.append(p);
 			gifDiv.append(animalImage);
 
-			$('#gifArea').prepend(gifDiv)
+			$('#gifArea').prepend(gifDiv);
 
 		}
 
-
 	});
+});
+
+$(document).on('click', 'img', function(){
+	var state = $(this).attr('data-state');
+
+	if ( state == 'still'){
+				$(this).attr('src', $(this).data('animate'));
+				$(this).attr('data-state', 'animate');
+	}
+	else{
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
+	}
+
+
+});
+
+$('#submitButton').on('click', function(){
+	var input = $('#searchInput').val().trim();
+	console.log(input);
+
+	var newButton = $('<button>').text(input);
+	newButton.attr('class', 'btn animalButton');
+	newButton.attr('data-animal', input)
+	$('#buttonArea').append(newButton);
+
+	return false;
+
 });
